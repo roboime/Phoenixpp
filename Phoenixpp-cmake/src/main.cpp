af858e3e4b58a77633b215fb5ca1d14c94049451
ebert-hvm <ebert.hv.melo@gmail.com>
2024-01-21 23:39:25 -0300

    adcionado leitura de json

--- Phoenixpp-cmake/src/main.cpp
+++ Phoenixpp-cmake/src/main.cpp
@@ -1,9 +1,25 @@
 #include "cli.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
 
-using namespace std;
+using json = nlohmann::json;
 
 int main(){
-    Cli ui;
-    ui.start();
+    std::ifstream file("../configurations/settings.json");
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+    json config;
+    file >> config;
+
+    std::string ui = config["ui"];
+    if (ui == "cli"){
+        Cli cli;
+        cli.start();
+    }
+    else{
+        cout << "unrecognized UI configuration";
+    }
     return 0;
 }
