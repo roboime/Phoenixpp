c77d6c963c30c8807f6f4641eb5b353ba70d91b1
ebert-hvm <ebert.hv.melo@gmail.com>
2024-06-01 17:51:49 -0300

    adding factory and Agent derived classes on uml

--- src/base_component.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by ebert on 5/30/2024.
-//
-
-#include "Phoenixpp/base_component.h"
-
-namespace roboime {
-} // roboime
---
+++ tests/vision/test_udp_vision.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ebert on 5/30/2024.
+//
+
+#include "Phoenixpp/vision/udp_vision.h"
+#include <iostream>
+using std::cout;
+
+roboime::UdpVision::UdpVision(int i, int j) {
+    cout << "oi\n";
+}
+
